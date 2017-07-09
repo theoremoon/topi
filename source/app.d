32907031150e5caf4e20b6c4ac4b54ec7f496ae2
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-07-09 13:10:04 +0900

    Support +-*()

--- source/app.d
+++ source/app.d
@@ -34,22 +34,26 @@ class BinopAst : Ast {
 			this.op = op;
 		}
 		override void emit() {
+			this.left.emit;
+			write("\tpush rax\n");
+			this.right.emit;
+			write("\tpush rax\n");
 			if (op == '+') {
-				this.right.emit;
-				write("\tpush rax\n");
 				write("\tpop rbx\n");
-				this.left.emit;
-				
+				write("\tpop rax\n");
 				write("\tadd rax, rbx\n");
 				return;
 			}
 			else if (op == '-') {
-				this.right.emit;
-				write("\tpush rax\n");
 				write("\tpop rbx\n");
-				this.left.emit;
-				
+				write("\tpop rax\n");
 				write("\tsub rax, rbx\n");
+				return;
+			}
+			else if (op == '*') {
+				write("\tpop rbx\n");
+				write("\tpop rax\n");
+				write("\timul rbx\n");
 				return;
 			}
 			throw new Exception("Unknwon operator '%c'".format(op));
@@ -93,11 +97,15 @@ class Source {
 			}
 			return cnt;
 		}
-
+		uint get_with_skip(ref dchar c) {
+			skip_space();
+			return get(c);
+		}
 		void unget(dchar c) {
 			buf ~= c;
 		}
 }
+
 
 Ast read_number(Source src, int n) {
 	dchar c;
@@ -110,23 +118,67 @@ Ast read_number(Source src, int n) {
 	}
 	return new IntegerAst(n);
 }
-Ast read_expr(Source src) {
+Ast read_factor(Source src) {
 	dchar c;
-	src.skip_space();
-	src.get(c);
-	auto left = src.read_number(c-'0');
-
-	dchar op;
-	src.skip_space();
-	if (! src.get(op)) {
-		return left;
+	if (! src.get_with_skip(c)) {
+		return null;
 	}
+	if (c.isNumber) {
+		return src.read_number(c-'0');
+	}
+	if (c == '(') {
+		auto e = src.read_expr;
+		if (! e) {
+			throw new Exception("Meaningless parentheses");
+		}
+		if (! src.get_with_skip(c)) {
+			throw new Exception("Unterminated parentheses");
+		}
+		if (c != ')') {
+			throw new Exception("')' is expected but got '%c'".format(c));
+		}
+		return e;
 
-	src.skip_space();
-	src.get(c);
-	auto right = src.read_number(c-'0');
-
-	return new BinopAst(op, left, right);
+	}
+	throw new Exception("Unexpected character: '%c'".format(c));
+}
+Ast read_term(Source src) {
+	auto f1 = src.read_factor;
+	if (! f1) {
+		return null;
+	}
+	dchar c;
+	if (! src.get_with_skip(c)) {
+		return f1;
+	}
+	if (c != '*') {
+		src.unget(c);
+		return f1;
+	}
+	auto f2 = src.read_term;
+	if (! f2) {
+		throw new Exception("Incomplete term");
+	}
+	return new BinopAst(c, f1, f2);
+}
+Ast read_expr(Source src) {
+	auto t1 = src.read_term;
+	if (!t1) {
+		return null;
+	}
+	dchar c;
+	if (! src.get_with_skip(c)) {
+		return t1;
+	}
+	if (c != '+' && c != '-') {
+		src.unget(c);
+		return t1;
+	}
+	auto t2 = src.read_expr;
+	if (!t2) {
+		throw new Exception("Incomplete expr");
+	}
+	return new BinopAst(c, t1, t2);
 }
 
 void emit_nasm_head() {
