14a8aab3acc917f448930893d11421548070d781
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-07-09 11:13:27 +0900

    Craete test file

---
+++ test.sh
@@ -0,0 +1,43 @@
+
+compiler=topi
+asm='tmp.asm'
+obj='tmp.o'
+bin='tmp.out'
+function compile {
+	echo "$1" | $compiler  > $asm
+	if [ $? -ne 0 ]; then
+		echo "Failed to Compile $1"
+		exit
+	fi
+
+	nasm -f elf64 -o $obj $asm
+	if [ $? -ne 0 ]; then
+		echo "Failed to Assemble $asm"
+		exit
+	fi
+
+	gcc -o $bin $obj driver.c
+	if [ $? -ne 0 ]; then
+		echo "Failed to Compile $obj"
+		exit
+	fi
+}
+
+function test {
+	compile $1
+	r=$bin	
+	
+	if [ "$r" != "$2" ]; then
+		echo "Expected $2 but got $r"
+		exit
+	fi
+	echo "test passed $1 == $2"
+}
+
+dub build
+if [ $? -ne 0 ]; then
+	echo "Build Failed"
+	exit
+fi
+
+test '1000' '1000'
