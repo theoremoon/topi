7a2fc9ede71bdab5ba7f4e8ff67abce67d5bf652
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-07-19 20:44:35 +0900

    expand expression

--- source/topi/parse.d
+++ source/topi/parse.d
@@ -19,22 +19,48 @@ IntegerAst read_factor(Source src) {
 	return null;
 }
 
-ValueAst read_expr(Source src) {
-	auto left = src.read_factor;
-	if (!left) {
+int get_priority(Token op) {
+	switch (op.str) {
+		case "=":
+			return 1;
+		case "==":
+			return 2;
+		case "+": case "-":
+			return 3;
+		case "*":
+			return 4;
+		default:
+			break;
+	}
+	return 0;
+}
+ValueAst read_expr(Source src, int p = -1) {
+	// left hand
+	ValueAst left = src.read_factor;
+	if (! left) {
 		return null;
 	}
-	auto op = src.get;
-	if (!op) {
-		return left;
+
+	while (true) {
+		// operator (or return left)
+		auto op = src.get;
+		if (! op) {
+			break;
+		}
+		if (op.type != Token.Type.SYMBOL) {
+			src.unget(op);
+			break;
+		}
+		int priority = op.get_priority;
+		if (priority == 0 || priority <= p) {
+			src.unget(op);
+			break;
+		}
+		auto right = src.read_expr(priority);
+		if (! right) {
+			throw new Exception("Right hand expression is required. line %d".format(src.line));
+		}
+		left = new OperatorAst(op.str, [left, right]);
 	}
-	if (op.type != Token.Type.SYMBOL) {
-		src.unget(op);
-		return left;
-	}
-	auto right = src.read_factor;
-	if (!right) {
-		throw new Exception("Error at %d: righthand factor is required".format(src.line));
-	}
-	return new OperatorAst(op.str, [left, right]);
+	return left;
 }
